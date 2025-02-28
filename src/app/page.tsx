db6d0b0e9d4cc40c2594ec4f8ed851e744013f8f
Pebble <pebbl.io@outlook.com>
2025-02-28 11:03:29 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,9 @@
 
 import { Section, Cell, Image, List } from '@telegram-apps/telegram-ui';
 import { useTranslations } from 'next-intl';
-
 import { Link } from '@/components/Link/Link';
 import { LocaleSwitcher } from '@/components/LocaleSwitcher/LocaleSwitcher';
 import { Page } from '@/components/Page';
-
 import tonSvg from './_assets/ton.svg';
 
 export default function Home() {
