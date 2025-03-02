0563851c3586b2f913b7eccf114e9f9f4da87298
Pebble-io <pebbl.io@outlook.com>
2025-03-02 16:56:23 +0200

    0.12.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,35 +7,80 @@ import BottomBar from '@/components/BottomBar';
 import Artilith_logo from '../app/_assets/Artilith_logo-no-bg.png';
 
 export default function Home() {
+  useEffect(() => {
+    const imgWrap = document.querySelector(".imgWrap");
+
+    if (imgWrap) {
+      const handleClick = () => {
+        imgWrap.classList.add("fast-glitch");
+
+        setTimeout(() => {
+          imgWrap.classList.remove("fast-glitch");
+        }, 300);
+      };
+
+      imgWrap.addEventListener("click", handleClick);
+
+      return () => {
+        imgWrap.removeEventListener("click", handleClick);
+      };
+    }
+  }, []);
+
   return (
     <Page back={false}>
       <List>
         <TopBar />
         <div className="HIJtihMA8FHczS02iWF5">
-          <Placeholder 
-            >
+          <Placeholder>
             <svg className="filter">
               <filter id="alphaRed">
-                <feColorMatrix mode="matrix" values="1 0 0 0 0  0 0 0 0 0  0 0 0 0 0  0 0 0 1 0" result="joint" />
+                <feColorMatrix
+                  mode="matrix"
+                  values="1 0 0 0 0  0 0 0 0 0  0 0 0 0 0  0 0 0 1 0"
+                  result="joint"
+                />
               </filter>
               <filter id="alphaGreen">
-                <feColorMatrix mode="matrix" values="0 0 0 0 0  0 1 0 0 0  0 0 0 0 0  0 0 0 1 0" result="joint" />
+                <feColorMatrix
+                  mode="matrix"
+                  values="0 0 0 0 0  0 1 0 0 0  0 0 0 0 0  0 0 0 1 0"
+                  result="joint"
+                />
               </filter>
               <filter id="alphaBlue">
-                <feColorMatrix mode="matrix" values="0 0 0 0 0  0 0 0 0 0  0 0 1 0 0  0 0 0 1 0" result="joint" />
+                <feColorMatrix
+                  mode="matrix"
+                  values="0 0 0 0 0  0 0 0 0 0  0 0 1 0 0  0 0 0 1 0"
+                  result="joint"
+                />
               </filter>
               <filter id="alpha">
-                <feColorMatrix type="saturate" values="0"/>
+                <feColorMatrix type="saturate" values="0" />
               </filter>
             </svg>
             <div className="page">
               <h1>HOLD to decrypt !</h1>
               <h2>If you keep it you reap the rewards</h2>
               <div className="imgWrap">
-                <img className="red" src={Artilith_logo.src} alt="Artilith Logo Red"/>
-                <img className="green" src={Artilith_logo.src} alt="Artilith Logo Green"/>
-                <img className="blue" src={Artilith_logo.src} alt="Artilith Logo Blue"/>
-                <p className="text"><span>Decode . . .</span></p>
+                <img
+                  className="red"
+                  src={Artilith_logo.src}
+                  alt="Artilith Logo Red"
+                />
+                <img
+                  className="green"
+                  src={Artilith_logo.src}
+                  alt="Artilith Logo Green"
+                />
+                <img
+                  className="blue"
+                  src={Artilith_logo.src}
+                  alt="Artilith Logo Blue"
+                />
+                <p className="text">
+                  <span>Decode . . .</span>
+                </p>
               </div>
             </div>
           </Placeholder>
@@ -45,3 +90,4 @@ export default function Home() {
     </Page>
   );
 }
+
