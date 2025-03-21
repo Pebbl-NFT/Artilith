e8d0c5b990d9a6bc2b953f636921c9c6506140fd
Pebble-io <pebbl.io@outlook.com>
2025-03-21 23:00:57 +0200

    0.13.10

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,11 @@
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+
 const withNextIntl = createNextIntlPlugin("./src/core/i18n/i18n.ts");
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = {
-    env: {
-      NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-      NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    },
-};
-
-export default withNextIntl(nextConfig);
+export default withNextIntl({
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+  // Ваші інші налаштування
+});
