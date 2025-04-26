ad7ff3056ba744278e977386b65c8a0397685c56
Pebble-io <pebbl.io@outlook.com>
2025-04-26 17:09:46 +0300

    alpha 0.18.5

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -316,16 +316,12 @@ export default function HomePage() {
                   className="blur-target"
                   style={{
                     display: "grid",
-                    gridTemplateColumns: "repeat(1, 1fr)",
                     gap: "20px",
                     width: "100%",
                     maxWidth: "1200px",
                     margin: "0 auto",
                     filter: locked ? "blur(15px)" : "none",
                     transition: "filter 0.3s ease",
-                    pointerEvents: locked ?   "none" : "auto",
-                    opacity: locked ? 0.5 : 1,
-                    cursor: locked ? "block" : "auto",
                   }}
                 >
                   {/* Твої ItemCard-и */}
@@ -341,9 +337,7 @@ export default function HomePage() {
               </div>
             </div>
           </Placeholder>
-      </Page>
-
-
+        </Page>
         );
       case 'home':
         return (
