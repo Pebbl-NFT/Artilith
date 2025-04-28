1185ded7c3d20ee8ce3d260d196f7d719c176f79
Pebble-io <pebbl.io@outlook.com>
2025-04-29 01:41:20 +0300

    alpha 0.19

--- src/app/home/profile/page.tsx
+++ src/app/home/profile/page.tsx
@@ -105,7 +105,7 @@ export default function ProfilePage() {
             <span style={styles.gameBadge}>Telegram</span>
             <span style={styles.gameBadge}>Discord</span>
             <span style={styles.gameBadge}>Twitter</span>
-            <span style={styles.gameBadge}>v. alpha 0.18.9</span>
+            <span style={styles.gameBadge}>v. alpha 0.19</span>
           </div>
         </Card>
         </Placeholder>
