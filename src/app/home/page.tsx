48b4ad924357ba170178394589efb3f989ebe304
Pebble-io <pebbl.io@outlook.com>
2025-06-10 12:23:50 +0300

    alpha 0.29

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2238,7 +2238,7 @@ export default function HomePage() {
                 <div 
                     onClick={(e) => e.stopPropagation()}
                     style={{
-                    backgroundImage: `url('/bg/cardbgcommon.png')`,
+                    backgroundImage: `url('/bg/cardbgcommon.PNG')`,
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
                     backgroundRepeat: 'no-repeat',
@@ -2384,7 +2384,7 @@ export default function HomePage() {
                 <div
                   onClick={(e) => e.stopPropagation()}
                   style={{
-                  backgroundImage: `url('/bg/Cardbg.png')`,
+                  backgroundImage: `url('/bg/cardbgcommon.PNG')`,
                   backgroundSize: 'cover',
                   backgroundPosition: 'center',
                   backgroundRepeat: 'no-repeat',
