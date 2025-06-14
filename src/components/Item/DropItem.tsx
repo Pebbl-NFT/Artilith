6800dac9c130da1d47d4a75cf4372607d4feef42
Pebble-io <pebbl.io@outlook.com>
2025-06-14 14:09:57 +0300

    alpha 0.29.3

---
+++ src/components/Item/DropItem.tsx
@@ -0,0 +1,15 @@
+import ton from "../../app/_assets/item/ton.png";
+
+export const DropItems = [
+  {
+    item_id: 700,
+    type: "element",
+    rarity: 'common',
+    name: "Елемнт TON",
+    description: "0.00001 TON",
+    damage: 0,
+    defense: 0,
+    price: 50,
+    image: ton.src,
+  },
+];
--- src/components/Item/Items.tsx
+++ src/components/Item/Items.tsx
@@ -1,10 +1,12 @@
 import { WeaponItems } from "./WeaponItem";
 import { ShieldItems } from "./ShieldItem";
 import { ScrollItems } from "./ScrollItem";
+import { DropItems } from "./DropItem";
 
 
 export const AllItems = [
     ...WeaponItems,
     ...ShieldItems,
     ...ScrollItems,
+    ...DropItems,
 ];
