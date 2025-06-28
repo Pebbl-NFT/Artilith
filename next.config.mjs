8602741461a72665cf7472aa1b7481e62c76a69e
Pebble-io <pebbl.io@outlook.com>
2025-06-28 13:42:01 +0300

    alpha 0.30.9

--- next.config.mjs
+++ next.config.mjs
@@ -2,25 +2,22 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin("./src/core/i18n/i18n.ts");
 
-// Ми додаємо новий ключ 'images' до об'єкта з налаштуваннями
 export default withNextIntl({
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
-
-  // -- ОСЬ ВАШ НОВИЙ БЛОК --
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        // ВАЖЛИВО: Замініть на ваш hostname з помилки!
-        hostname: 'fdhyjbwqterimkmyfic.supabase.co', 
+        hostname: 'fohyijwxgtmrimknyfic.supabase.co', 
         port: '',
-        pathname: '/storage/v1/object/public/**',
+        // -- ОСЬ ЦЕЙ РЯДОК ПОТРІБНО ЗМІНИТИ --
+        // Було: '/storage/v1/object/public/**'
+        // Стало:
+        pathname: '/storage/v1/object/public/image-templates/**',
       },
     ],
   },
-  
-  // Ваші інші налаштування можуть бути тут
 });
